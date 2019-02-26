364decd8dc0fac6792e6cadeec60a6ec8d5d65f3
Raimana Lowgreen <raimana.lowgreen@propellerhead.co.nz>
2019-02-26 21:23:10 +1300

    init

---
+++ SyslogTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using NLog;
+using NLog.Config;
+
+namespace SyslogTest
+{
+    class Program
+    {
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static void Main(string[] args)
+        {
+            LogManager.Configuration = new XmlLoggingConfiguration($"{AppContext.BaseDirectory}nlog.config");
+
+            while (true)
+            {
+                Console.WriteLine("Enter message count...");
+                int messageCount = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= messageCount; i++)
+                {
+                    logger.Info($"Test log message {i}");
+                }
+            }
+        }
+    }
+}
+
